4b6acb3dfa0589db923d1cedfcd56af67d5d48d9
Sara <saralyon15@augustana.edu>
2016-08-31 13:59:54 -0500

    Started the new Yak application and printed bye.

--- TestProject/src/TestMain.java
+++ TestProject/src/TestMain.java
@@ -3,7 +3,8 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.print("Hello Emu World");
+	System.out.println("Hello Emu World");
+	System.out.println("Goodbye.");
 	}
 
 }
